1190b251977024309afb08beb85ff0bfb2503856
David A. Mellis <d.mellis@arduino.cc>
2011-05-04 11:30:21 -0400

    Updating speed / motion control settings.

--- settings.h
+++ settings.h
@@ -61,15 +61,15 @@ void settings_store_setting(int parameter, double value);
 //#define DEFAULT_Z_STEPS_PER_MM (94.488188976378*MICROSTEPS)
 
 // steps per revolution * revolutions per inch / mm per inch
-#define DEFAULT_X_STEPS_PER_MM (400.0 * 20.0 / 25.4)
-#define DEFAULT_Y_STEPS_PER_MM (400.0 * 20.0 / 25.4)
-#define DEFAULT_Z_STEPS_PER_MM (400.0 * 20.0 / 25.4)
-#define DEFAULT_STEP_PULSE_MICROSECONDS 30
+#define DEFAULT_X_STEPS_PER_MM (400.0 * 10.0 / 25.4)
+#define DEFAULT_Y_STEPS_PER_MM (400.0 * 10.0 / 25.4)
+#define DEFAULT_Z_STEPS_PER_MM (400.0 * 10.0 / 25.4)
+#define DEFAULT_STEP_PULSE_MICROSECONDS 10
 #define DEFAULT_MM_PER_ARC_SEGMENT 0.1
-#define DEFAULT_RAPID_FEEDRATE 225.0 // in millimeters per minute
-#define DEFAULT_FEEDRATE 225.0
+#define DEFAULT_RAPID_FEEDRATE 600.0 // in millimeters per minute
+#define DEFAULT_FEEDRATE 500.0
 //#define DEFAULT_ACCELERATION (DEFAULT_FEEDRATE/100.0)
-#define DEFAULT_ACCELERATION (3.0)
+#define DEFAULT_ACCELERATION (25.0)
 #define DEFAULT_MAX_JERK 225.0
 #define DEFAULT_STEPPING_INVERT_MASK 0
 
